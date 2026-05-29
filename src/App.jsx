5ab4f3f49b7e88ff4ddd312f2f987ab4fc46506e
Adarsh Goyal <adarshgoyal2007@gmail.com>
2026-05-29 16:59:48 -0700

    first project

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react"
+import 'React'
+
+const App = () => {
+
+  const [num,setFirst]=useState(0)
+
+  function IncreaseNum(){
+    setFirst(num+1)
+
+  }
+
+  function DecreaseNum(){
+    setFirst(num-1)
+
+  }
+
+  function Increase5(){
+    setFirst(num+5)
+  }
+  
+  return (
+    <div>
+    <h1>{num}</h1>
+    <button onClick={IncreaseNum}>Increase</button>
+    <button onClick={DecreaseNum}>Decrease</button>
+    <button onClick={Increase5}>Increase 5</button>
+    </div>
+  )
+}
+
+export default App
